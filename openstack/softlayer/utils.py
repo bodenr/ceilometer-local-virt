40601e9dd0d615acd15136b5cfa09575ede54e08
boden <boden@linux.vnet.ibm.com>
2014-07-30 11:48:13 -0400

    update requirements and return ips

--- openstack/softlayer/utils.py
+++ openstack/softlayer/utils.py
@@ -7,8 +7,8 @@ def hostname():
 
 def private_ip():
     # SoftLayer private always on eth0
-    netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+    return netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
 
 def public_ip():
     # SoftLayer public always on eth1
-    netifaces.ifaddresses('eth1')[netifaces.AF_INET][0]['addr']
+    return netifaces.ifaddresses('eth1')[netifaces.AF_INET][0]['addr']
